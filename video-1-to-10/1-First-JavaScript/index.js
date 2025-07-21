950f785469bd1b32ea9299cc177964ceee3d754a
Ahmadreza <ahmadrezafooladi1999@gmail.com>
2025-07-21 21:32:10 +0330

    Javascript-tutorial-files

---
+++ video-1-to-10/1-First-JavaScript/index.js
@@ -0,0 +1,20 @@
+console.log(`Hello`);
+console.log(`I like pizza!`);
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
+
+document.getElementById("myH12").textContent = `I Tired Man `;
+document.getElementById("myp2").textContent = `Today I Start To Learning JavaScript Language, Man That So Hard`;
+
+// This is a comment
+
+/*
+    This
+    is
+    a
+    comment
+*/
---
+++ video-1-to-10/2-VARIABLES/index.js
@@ -0,0 +1,10 @@
+// variable = A container that stores a value.
+//                   Behaves as if it were the value it contains.
+
+let fullName = "Ahmadreza16";
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
